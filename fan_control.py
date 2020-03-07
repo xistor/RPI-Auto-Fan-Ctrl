89100b3f14170e6c58dda82ab182f2d0b73abb12
xueluqiang <xueluqiang@gmail.com>
2020-03-07 22:00:03 +0800

    modify speed calc

--- fan_control.py
+++ fan_control.py
@@ -18,24 +18,22 @@ piFan.start()
 def getCpuTemperature():
     res = os.popen('vcgencmd measure_temp').readline()
     temp =(res.replace("temp=","").replace("'C\n",""))
-    print("temp is {0}".format(temp))
+    #print("temp is {0}".format(temp))
     return float(temp)
 
 while True:
+    time.sleep(30)
     temp = getCpuTemperature()
     speed = FAN_LOW + (FAN_HIGH - FAN_LOW) * ((temp - MIN_TEMP) / (MAX_TEMP - MIN_TEMP))
     if speed > 100.0:
         speed = 100
-    print("Speed : {}".format(speed))
+    #print("Speed : {}".format(speed))
 
     if temp < MIN_TEMP:
         piFan.setSpeed(0)
-        print("stop")
-    elif temp > MAX_TEMP:
-        piFan.setSpeed(100)
-        print("MAX")
+        #print("stop")
     elif temp > ADJ_TEMP:
         piFan.setSpeed(speed)
-        print("adj")
-    time.sleep(30)
+        #print("adj")
+    
 
